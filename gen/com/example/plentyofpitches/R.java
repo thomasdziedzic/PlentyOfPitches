8b885f94703fd0b3594f8bccca6b40249f2889e3
Thomas Dziedzic <gostrc@gmail.com>
2013-06-13 23:33:29 -0700

    add listview to browse item activity

--- gen/com/example/plentyofpitches/R.java
+++ gen/com/example/plentyofpitches/R.java
@@ -25,17 +25,18 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000b;
-        public static final int button_browse=0x7f080006;
-        public static final int button_new=0x7f080005;
-        public static final int button_search=0x7f080007;
-        public static final int delete_button=0x7f080003;
-        public static final int description=0x7f080009;
-        public static final int itemType=0x7f080008;
-        public static final int save_button=0x7f08000a;
-        public static final int switch1=0x7f080004;
+        public static final int action_settings=0x7f08000c;
+        public static final int button_browse=0x7f080007;
+        public static final int button_new=0x7f080006;
+        public static final int button_search=0x7f080008;
+        public static final int delete_button=0x7f080004;
+        public static final int description=0x7f08000a;
+        public static final int itemType=0x7f080009;
+        public static final int listView1=0x7f080002;
+        public static final int save_button=0x7f08000b;
+        public static final int switch1=0x7f080005;
         public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_browse_item=0x7f030000;
