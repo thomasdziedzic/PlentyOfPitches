71e31a7e929e339dda8523076738f3c71bd1904a
Thomas Dziedzic <gostrc@gmail.com>
2013-06-22 23:22:02 -0700

    use android-async-http in new item activity

--- src/com/example/plentyofpitches/NewItemActivity.java
+++ src/com/example/plentyofpitches/NewItemActivity.java
@@ -1,24 +1,14 @@
 package com.example.plentyofpitches;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
@@ -26,6 +16,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class NewItemActivity extends Activity {
 	private String urlItem;
@@ -81,84 +74,36 @@ public class NewItemActivity extends Activity {
 	}
 
 	public void save(View view) throws JSONException, IOException {
-		CreateItem google = new CreateItem(this, urlItem);
-		google.execute("http://50.116.4.81:5000/" + urlItem);
-	}
-	
-	public class CreateItem extends AsyncTask<String, Void, String> {
-		Context ctx;
-		String itemType;
-		
-		CreateItem(Context ctx, String itemType) {
-			super();
-			
-			this.ctx = ctx;
-			this.itemType = itemType;
+		EditText descriptionEditText = (EditText) findViewById(R.id.description);
+		String description = descriptionEditText.getText().toString();
+
+		JSONObject requestObject = new JSONObject();
+		try {
+			requestObject.put("description", description);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		protected String doInBackground(String... urls) {
-			String url = urls[0];
-			
-			HttpClient client = new DefaultHttpClient();
-			HttpPost post = new HttpPost(url);
-			
-			String ret = "";
-			
-			EditText descriptionEditText = (EditText) findViewById(R.id.description);
-			String description = descriptionEditText.getText().toString();
-
-			JSONObject requestObject = new JSONObject();
-			try {
-				requestObject.put("description", description);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String requestString = requestObject.toString();
-			
-			try {
-				post.setEntity(new StringEntity(requestString));
-			} catch (UnsupportedEncodingException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			try {
-				HttpResponse response = client.execute(post);
-				
-				StatusLine statusLine = response.getStatusLine();
-				int statusCode = statusLine.getStatusCode();
-				
-				if (statusCode == 200) {
-					String line = "";
-
-					BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-					while ((line = rd.readLine()) != null) {
-	                    ret += line;
-	                }
+		String requestString = requestObject.toString();
+		
+		final Context self = this;
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.post(this, "http://50.116.4.81:5000/" + urlItem, new StringEntity(requestString), "application/json", new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response) {
+				try {
+					JSONObject jObject = new JSONObject(response);
+					int id = jObject.getInt("id");
+					
+					Intent intent = new Intent(self, ExistingItemActivity.class);
+					intent.putExtra(MainActivity.ITEM_TYPE_KEY, urlItem);
+					intent.putExtra(NewItemActivity.ITEM_ID_KEY, id);
+			    	startActivity(intent);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			} catch (ClientProtocolException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-			
-			return ret;
-		}
-		
-		protected void onPostExecute(String result) {
-			try {
-				JSONObject jObject = new JSONObject(result);
-				int id = jObject.getInt("id");
-				
-				Intent intent = new Intent(ctx, ExistingItemActivity.class);
-				intent.putExtra(MainActivity.ITEM_TYPE_KEY, this.itemType);
-				intent.putExtra(NewItemActivity.ITEM_ID_KEY, id);
-		    	startActivity(intent);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-		}
+		});
 	}
-	
 }
