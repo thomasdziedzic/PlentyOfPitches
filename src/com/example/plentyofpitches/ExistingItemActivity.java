4a3269c64985b556523685b27bd9a66802053a2e
Thomas Dziedzic <gostrc@gmail.com>
2013-06-22 23:14:22 -0700

    use android-async-http in existing item activity

--- src/com/example/plentyofpitches/ExistingItemActivity.java
+++ src/com/example/plentyofpitches/ExistingItemActivity.java
@@ -1,29 +1,19 @@
 package com.example.plentyofpitches;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class ExistingItemActivity extends Activity {
 	private int id;
@@ -39,9 +29,23 @@ public class ExistingItemActivity extends Activity {
 		Intent intent = getIntent();
 		itemType = intent.getStringExtra(MainActivity.ITEM_TYPE_KEY);
 		id = intent.getIntExtra(NewItemActivity.ITEM_ID_KEY, 0);
-		
-		ReadItem readItem = new ReadItem(this);
-		readItem.execute("http://50.116.4.81:5000/" + itemType + "/" + id);
+
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("http://50.116.4.81:5000/" + itemType + "/" + id, new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response) {
+				try {
+					JSONObject jObject = new JSONObject(response);
+					String description = jObject.getString("description");
+					
+					TextView editText = (TextView) findViewById(R.id.textView2);
+					editText.setText(description);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	/**
@@ -78,104 +82,12 @@ public class ExistingItemActivity extends Activity {
 	}
 	
 	public void delete(View view) {
-		DeleteItem deleteItem = new DeleteItem(this);
-		deleteItem.execute("http://50.116.4.81:5000/" + itemType + "/" + id);
-	}
-	
-	public class DeleteItem extends AsyncTask<String, Void, String> {
-		Context ctx;
-		
-		DeleteItem(Context ctx) {
-			super();
-			
-			this.ctx = ctx;
-		}
-		protected String doInBackground(String... urls) {
-			String url = urls[0];
-			
-			HttpClient client = new DefaultHttpClient();
-			HttpDelete delete = new HttpDelete(url);
-			
-			String ret = "";
-			
-			try {
-				HttpResponse response = client.execute(delete);
-				
-				StatusLine statusLine = response.getStatusLine();
-				int statusCode = statusLine.getStatusCode();
-				
-				if (statusCode == 200) {
-					String line = "";
-
-					BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-					while ((line = rd.readLine()) != null) {
-	                    ret += line;
-	                }
-				}
-			} catch (ClientProtocolException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-			
-			return ret;
-		}
-		
-		protected void onPostExecute(String result) {
-			finish();
-		}
-	}
-
-	public class ReadItem extends AsyncTask<String, Void, String> {
-		Context ctx;
-		
-		ReadItem(Context ctx) {
-			super();
-			
-			this.ctx = ctx;
-		}
-		protected String doInBackground(String... urls) {
-			String url = urls[0];
-			
-			HttpClient client = new DefaultHttpClient();
-			HttpGet get = new HttpGet(url);
-			
-			String ret = "";
-			
-			try {
-				HttpResponse response = client.execute(get);
-				
-				StatusLine statusLine = response.getStatusLine();
-				int statusCode = statusLine.getStatusCode();
-				
-				if (statusCode == 200) {
-					String line = "";
-
-					BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-					while ((line = rd.readLine()) != null) {
-	                    ret += line;
-	                }
-				}
-			} catch (ClientProtocolException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-			
-			return ret;
-		}
-		
-		protected void onPostExecute(String result) {
-			try {
-				JSONObject jObject = new JSONObject(result);
-				String description = jObject.getString("description");
-				
-				TextView editText = (TextView) findViewById(R.id.textView2);
-				editText.setText(description);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.delete("http://50.116.4.81:5000/" + itemType + "/" + id, new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response) {
+				finish();
 			}
-		}
+		});
 	}
 }
